5882761afca506a827fda0cc15112d67f465f381
amna <othmanalialamin@gmail.com>
2022-04-24 16:20:11 -0700

    modified currentday

--- Develop/main.js
+++ Develop/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {// tells engine to load 1)html & 2)css first.
     //display current day & time.
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    $("#currentDay").text(moment().format("dddd, MMMM Mo")); // use of moment.js
     //assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
         //get nearby values.
